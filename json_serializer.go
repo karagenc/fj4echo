117f70f446d9752a747fca87fc99263ccfe4f0ba
tomruk <41700170+tomruk@users.noreply.github.com>
2023-02-11 19:11:35 +0300

    Add support for configuration
    Configuration is customizable. Also improved and fixed the go build
    directive.

--- json_serializer.go
+++ json_serializer.go
@@ -1,4 +1,4 @@
-//go:build !amd64
+//go:build !amd64 || (amd4 && !(linux || windows || darwin))
 
 package fj4echo
 
@@ -11,29 +11,36 @@ import (
 )
 
 func New() echo.JSONSerializer {
-	return new(goJSONSerializer)
+	defaultConfig := DefaultConfig()
+	return &goJSONSerializer{config: defaultConfig.GoJSON}
+}
+
+func NewWithConfig(config Config) echo.JSONSerializer {
+	return &goJSONSerializer{config: config.GoJSON}
 }
 
 func Type() SerializerType {
 	return SerializerTypeGoJSON
 }
 
-// goJSONSerializer implements JSON encoding using github.com/goccy/go-json.
-type goJSONSerializer struct{}
+// goJSONSerializer implements JSON encoding using github.com/goccy/go-json
+type goJSONSerializer struct {
+	config GoJSONConfig
+}
 
 // Serialize converts an interface into a json and writes it to the response.
 // You can optionally use the indent parameter to produce pretty JSONs.
-func (d goJSONSerializer) Serialize(c echo.Context, i interface{}, indent string) error {
+func (s *goJSONSerializer) Serialize(c echo.Context, i interface{}, indent string) error {
 	enc := json.NewEncoder(c.Response())
 	if indent != "" {
 		enc.SetIndent("", indent)
 	}
-	return enc.Encode(i)
+	return enc.EncodeWithOption(i, s.config.EncodeOptions...)
 }
 
 // Deserialize reads a JSON from a request body and converts it into an interface.
-func (d goJSONSerializer) Deserialize(c echo.Context, i interface{}) error {
-	err := json.NewDecoder(c.Request().Body).Decode(i)
+func (s *goJSONSerializer) Deserialize(c echo.Context, i interface{}) error {
+	err := json.NewDecoder(c.Request().Body).DecodeWithOption(i, s.config.DecodeOptions...)
 	if ute, ok := err.(*json.UnmarshalTypeError); ok {
 		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Unmarshal type error: expected=%v, got=%v, field=%v, offset=%v", ute.Type, ute.Value, ute.Field, ute.Offset)).SetInternal(err)
 	} else if se, ok := err.(*json.SyntaxError); ok {
--- json_serializer_amd64.go
+++ json_serializer_amd64.go
@@ -12,20 +12,27 @@ import (
 )
 
 func New() echo.JSONSerializer {
-	return new(sonicJSONSerializer)
+	defaultConfig := DefaultConfig()
+	return &sonicJSONSerializer{defaultConfig.SonicConfig.Froze()}
+}
+
+func NewWithConfig(config Config) echo.JSONSerializer {
+	return &sonicJSONSerializer{api: config.SonicConfig.Froze()}
 }
 
 func Type() SerializerType {
 	return SerializerTypeSonic
 }
 
-// sonicJSONSerializer implements JSON encoding using github.com/bytedance/sonic.
-type sonicJSONSerializer struct{}
+// sonicJSONSerializer implements JSON encoding using github.com/bytedance/sonic
+type sonicJSONSerializer struct {
+	api sonic.API
+}
 
 // Serialize converts an interface into a json and writes it to the response.
 // You can optionally use the indent parameter to produce pretty JSONs.
-func (d sonicJSONSerializer) Serialize(c echo.Context, i interface{}, indent string) error {
-	enc := sonic.ConfigDefault.NewEncoder(c.Response())
+func (s sonicJSONSerializer) Serialize(c echo.Context, i interface{}, indent string) error {
+	enc := s.api.NewEncoder(c.Response())
 	if indent != "" {
 		enc.SetIndent("", indent)
 	}
@@ -33,8 +40,8 @@ func (d sonicJSONSerializer) Serialize(c echo.Context, i interface{}, indent str
 }
 
 // Deserialize reads a JSON from a request body and converts it into an interface.
-func (d sonicJSONSerializer) Deserialize(c echo.Context, i interface{}) error {
-	err := sonic.ConfigFastest.NewDecoder(c.Request().Body).Decode(i)
+func (s sonicJSONSerializer) Deserialize(c echo.Context, i interface{}) error {
+	err := s.api.NewDecoder(c.Request().Body).Decode(i)
 	if ute, ok := err.(*json.UnmarshalTypeError); ok {
 		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Unmarshal type error: expected=%v, got=%v, field=%v, offset=%v", ute.Type, ute.Value, ute.Field, ute.Offset)).SetInternal(err)
 	} else if se, ok := err.(*json.SyntaxError); ok {
